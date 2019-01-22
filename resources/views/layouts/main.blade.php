bf2310504ea534cf8f15864dcd41e95fc39114b5
Marcos <marcoscav@gmail.com>
2019-01-22 09:57:27 -0300

    nada

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+    <title>EasyLab</title>
+    <link href="/css/app.css" rel="stylesheet">
+  </head>
+  <body>
+
+  </body>
+</html>
